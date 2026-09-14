e9e0aaa7234c0da35006d7c5162e7e9aaa6c62e1
Tosty144 <jhosepvaron@gmail.com>
2026-09-14 13:00:59 -0300

    corregir error de cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors'); // 1. Importas cors
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
+app.use(cors()); // 2. Habilitas cors (siempre antes de tus rutas)
 app.use(express.json());
-app.use('/api', userRoutes);
+app.use('/api', userRoutes); // Tus rutas originales se mantienen aquí
 
 module.exports = app;
