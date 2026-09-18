37bf458900a342f8078b7700364a657b2fcdd145
Tosty144 <jhosepvaron@gmail.com>
2026-09-18 16:31:52 -0300

    Fix: corrección de carga de main.js y eventos de botones

--- src/app.js
+++ src/app.js
@@ -6,13 +6,13 @@ const app = express();
 
 app.use(express.json());
 
-// 1. Servir archivos estáticos (index.html, main.js, etc.) desde la raíz del proyecto
+// Servir la raíz del proyecto (para index.html)
 app.use(express.static(path.join(__dirname, '../')));
 
-// 2. Rutas de la API
+// Rutas de la API
 app.use('/api', userRoutes);
 
-// 3. Ruta principal para enviar index.html explícitamente cuando entren a http://localhost:3000/
+// Entregar index.html en la raíz
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'));
 });
